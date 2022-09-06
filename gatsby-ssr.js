f198152fbfd3bb8717b0e8613764bdd89f24caa8
Braian Mellor <braianj@gmail.com>
2022-09-06 15:11:19 -0300

    fix: run prettier in root path

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,28 +5,23 @@
  */
 
 // You can delete this file if you're not using it
-import React from 'react'
-import Segment from 'decentraland-gatsby/dist/components/Development/Segment'
-import Rollbar from 'decentraland-gatsby/dist/components/Development/Rollbar'
-export { wrapPageElement, wrapRootElement } from './gatsby-browser'
+import React from "react"
+
+import Rollbar from "decentraland-gatsby/dist/components/Development/Rollbar"
+import Segment from "decentraland-gatsby/dist/components/Development/Segment"
+export { wrapPageElement, wrapRootElement } from "./gatsby-browser"
 
 /**
  * @see https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/#onPreRenderHTML
  */
-export function onPreRenderHTML(
-  {
-    pathname,
-    getHeadComponents,
-    replaceHeadComponents,
-    getPreBodyComponents,
-    replacePreBodyComponents,
-    getPostBodyComponents,
-    replacePostBodyComponents,
-  },
-  pluginOptions
-) {
+export function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+  getPostBodyComponents,
+  replacePostBodyComponents,
+}) {
   const headComponents = getHeadComponents().map((component) => {
-    if (component.type !== 'style' || !component.props['data-href']) {
+    if (component.type !== "style" || !component.props["data-href"]) {
       return component
     }
 
@@ -34,7 +29,7 @@ export function onPreRenderHTML(
       <link
         rel="stylesheet"
         id={component.props.id}
-        href={component.props['data-href']}
+        href={component.props["data-href"]}
       />
     )
   })
@@ -50,13 +45,13 @@ export function onPreRenderHTML(
       />
     )
   } else {
-    console.warn('Missing GATSBY_SEGMENT_KEY environment')
+    console.warn("Missing GATSBY_SEGMENT_KEY environment")
   }
 
   if (process.env.GATSBY_ROLLBAR_TOKEN) {
     postBodyComponents.push(<Rollbar key="rollbar" />)
   } else {
-    console.warn('Missing GATSBY_ROLLBAR_TOKEN environment')
+    console.warn("Missing GATSBY_ROLLBAR_TOKEN environment")
   }
 
   replaceHeadComponents(headComponents)
