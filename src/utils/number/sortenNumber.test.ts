f4898ffd4e38ca4f905f0e58c28bb18da4818a91
Braian Mellor <braianj@gmail.com>
2022-09-06 20:16:19 -0300

    feat: new component LikeBox

---
+++ src/utils/number/sortenNumber.test.ts
@@ -0,0 +1,25 @@
+import shorterNumber from "../../utils/number/sortenNumber"
+
+describe("Sorten Number", () => {
+  test(`With 100`, () => {
+    expect(shorterNumber(100)).toBe("100")
+  })
+  test(`With 1.000`, () => {
+    expect(shorterNumber(1000)).toBe("1k")
+  })
+  test(`With 1.000.000`, () => {
+    expect(shorterNumber(1000000)).toBe("1M")
+  })
+  test(`With 1.000.000.000`, () => {
+    expect(shorterNumber(1000000000)).toBe("1G")
+  })
+  test(`With 1.000.000.000.000`, () => {
+    expect(shorterNumber(1000000000000)).toBe("1T")
+  })
+  test(`With 1.000.000.000.000.000`, () => {
+    expect(shorterNumber(1000000000000000)).toBe("1P")
+  })
+  test(`With 1.000.000.000.000.000.000`, () => {
+    expect(shorterNumber(1000000000000000000)).toBe("1E")
+  })
+})
