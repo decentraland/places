2aa2317df6a7c27552eee8470400a90fa6ff6e64
Braian Mellor <braianj@gmail.com>
2022-09-07 00:48:05 -0300

    feat: new component DislikeBox

---
+++ src/components/Button/DislikeBox.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+
+import { Button } from "decentraland-ui/dist/components/Button/Button"
+import Icon from "semantic-ui-react/dist/commonjs/elements/Icon"
+import Label from "semantic-ui-react/dist/commonjs/elements/Label"
+
+import shorterNumber from "../../utils/number/sortenNumber"
+
+import "./ButtonBox.css"
+
+export type DislikeBoxProps = {
+  total: number
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  loading?: boolean
+  active?: boolean
+}
+
+export default React.memo(function DislikeBox(props: DislikeBoxProps) {
+  const { onClick, loading, active, total } = props
+  return (
+    <Button
+      secondary
+      onClick={onClick}
+      className="button-box icon"
+      target="_blank"
+      loading={loading}
+      disabled={loading}
+    >
+      <Icon
+        name="thumbs down outline"
+        className={active ? "button-box__icon" : "button-box__icon-hovered"}
+      />
+
+      <Icon
+        name="thumbs down"
+        className={!active ? "button-box__icon" : "button-box__icon-hovered"}
+      />
+      <Label>{shorterNumber(total)}</Label>
+    </Button>
+  )
+})
