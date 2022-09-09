820ab42ab01ed446f4e03539223c1fc2be7b96d3
Braian Mellor <braianj@gmail.com>
2022-09-09 13:54:27 -0300

    feat: deeply import memo from radash
    Co-authored-by: frami <i@2fd.me>

--- src/modules/servers.ts
+++ src/modules/servers.ts
@@ -1,4 +1,4 @@
 import Catalyst from "decentraland-gatsby/dist/utils/api/Catalyst"
-import { memo } from "radash"
+import { memo } from "radash/dist/curry"
 
 export const getServers = memo(async () => Catalyst.get().getServers())
