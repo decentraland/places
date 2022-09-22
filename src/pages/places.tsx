bb69607cd1227635a1018b17496728bc4f058472
frami <i@2fd.me>
2022-09-22 13:19:41 -0300

    wip: places

--- src/pages/places.tsx
+++ src/pages/places.tsx
@@ -4,12 +4,15 @@ import { useLocation } from "@gatsbyjs/reach-router"
 import useAuthContext from "decentraland-gatsby/dist/context/Auth/useAuthContext"
 import useAsyncState from "decentraland-gatsby/dist/hooks/useAsyncState"
 import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
+import { Card } from "decentraland-ui/dist/components/Card/Card"
 import { Container } from "decentraland-ui/dist/components/Container/Container"
 import Grid from "semantic-ui-react/dist/commonjs/collections/Grid"
 
 import Places from "../api/Places"
 import Navigation, { NavigationTab } from "../components/Layout/Navigation"
 import PlaceCard from "../components/Place/PlaceCard/PlaceCard"
+
+import "./places.css"
 
 export default function IndexPage() {
   const l = useFormatMessage()
@@ -23,17 +26,21 @@ export default function IndexPage() {
   return (
     <>
       <Navigation activeTab={NavigationTab.Places} />
-      <Grid stackable>
-        <Grid.Row></Grid.Row>
+      <Grid stackable className="places-page">
+        <Grid.Row>
+          <Grid.Column
+            tablet={4}
+            className="places-page__filters"
+          ></Grid.Column>
+          <Grid.Column tablet={12} className="places-page__list">
+            <Card.Group>
+              {(places || []).map((place) => (
+                <PlaceCard place={place} />
+              ))}
+            </Card.Group>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </>
-  )
-
-  return (
-    <Container>
-      {(places || []).map((place) => (
-        <PlaceCard place={place} />
-      ))}
-    </Container>
   )
 }
