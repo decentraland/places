9373f52f638fafa5d2e4e58e69d1b8c567c7c71e
frami <i@2fd.me>
2022-09-27 11:13:06 -0300

    fix: activity summary

--- src/entities/PlaceActivityDaily/model.ts
+++ src/entities/PlaceActivityDaily/model.ts
@@ -8,10 +8,12 @@ export default class PlaceActivityModel extends Model<PlaceActivityDailyAttribut
   static withTimestamps = false
 
   static async findLatest(): Promise<Date | null> {
-    const sql = SQL`SELECT "date" FROM ${table(this)} ORDER BY "date" LIMIT 1`
-    const dates: [Date][] = await this.query(sql)
-    if (dates && dates[0] && dates[0][0]) {
-      return dates[0][0]
+    const sql = SQL`SELECT "date" FROM ${table(
+      this
+    )} ORDER BY "date" DESC LIMIT 1`
+    const dates: { date: Date }[] = await this.namedQuery("find_lates", sql)
+    if (dates && dates[0]) {
+      return dates[0].date
     }
 
     return null
