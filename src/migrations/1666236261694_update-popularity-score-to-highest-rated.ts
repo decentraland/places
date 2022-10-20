d1087ab1cde211697727f44d9e25a4ba0cdb0afb
Braian Mellor <braianj@gmail.com>
2022-10-20 18:43:04 -0300

    feat: update migration to set all like_rate to 0

--- src/migrations/1666236261694_update-popularity-score-to-highest-rated.ts
+++ src/migrations/1666236261694_update-popularity-score-to-highest-rated.ts
@@ -5,6 +5,7 @@ import PlaceModel from "../entities/Place/model"
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.db.query(`UPDATE places set "popularity_score" = 0`)
   pgm.renameColumn(PlaceModel.tableName, "popularity_score", "like_rate")
 }
 
