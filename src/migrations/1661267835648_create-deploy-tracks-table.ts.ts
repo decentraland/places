3e813715b1a672a181065cfee48c795b8fbe4cf4
Braian Mellor <braianj@gmail.com>
2022-10-26 20:12:19 -0300

    fix: wrong change in old migration

--- src/migrations/1661267835648_create-deploy-tracks-table.ts.ts
+++ src/migrations/1661267835648_create-deploy-tracks-table.ts.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Type } from "decentraland-gatsby/dist/entities/Database/types"
 import Catalyst from "decentraland-gatsby/dist/utils/api/Catalyst"
 import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate"
@@ -6,12 +7,6 @@ import isURL from "validator/lib/isURL"
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropTable("deployment_tracks", { cascade: true })
-
-  pgm.dropTable("entities_places", { cascade: true })
-}
-
-export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.createTable("deployment_tracks", {
     id: {
       type: Type.TransactionHash,
@@ -75,15 +70,8 @@ export async function down(pgm: MigrationBuilder): Promise<void> {
       pgm.sql(sql)
     }
   }
+}
 
-  pgm.createTable("entities_places", {
-    place_id: {
-      type: Type.UUID,
-      primaryKey: true,
-    },
-    entity_id: {
-      type: Type.Text,
-      primaryKey: true,
-    },
-  })
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("deployment_tracks", { cascade: true })
 }
