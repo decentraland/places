f0f17df5736a29c45521142e854216340ea7cfe2
Braian Mellor <braianj@gmail.com>
2022-10-28 15:25:32 -0300

    fix: restore siteUrl function

--- src/entities/Place/utils.ts
+++ src/entities/Place/utils.ts
@@ -18,6 +18,12 @@ const DECENTRALAND_URL =
   process.env.DECENTRALAND_URL ||
   "https://play.decentraland.org"
 
+export function siteUrl(pathname = "") {
+  const target = new URL(DECENTRALAND_URL)
+  target.pathname = pathname
+  return target
+}
+
 export function placeTargetUrl(
   place: Pick<PlaceAttributes, "base_position">,
   realm?: string
