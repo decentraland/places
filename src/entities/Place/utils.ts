023672781094c1d91b9a8f41aa961f1d76ea9638
Braian Mellor <braianj@gmail.com>
2022-11-01 18:15:22 -0300

    refactor: move utils to migration and update migration test

--- src/entities/Place/utils.ts
+++ src/entities/Place/utils.ts
@@ -1,13 +1,8 @@
-import API from "decentraland-gatsby/dist/utils/api/API"
-import Catalyst from "decentraland-gatsby/dist/utils/api/Catalyst"
 import {
   EntityScene,
   HotScene,
 } from "decentraland-gatsby/dist/utils/api/Catalyst.types"
 import Land from "decentraland-gatsby/dist/utils/api/Land"
-import Time from "decentraland-gatsby/dist/utils/date/Time"
-import env from "decentraland-gatsby/dist/utils/env"
-import { v4 as uuid } from "uuid"
 
 import {
   AggregatePlaceAttributes,
@@ -19,8 +14,6 @@ const DECENTRALAND_URL =
   process.env.GATSBY_DECENTRALAND_URL ||
   process.env.DECENTRALAND_URL ||
   "https://play.decentraland.org"
-
-const PLACES_URL = env("PLACES_URL", "https://places.decentraland.org")
 
 export function siteUrl(pathname = "") {
   const target = new URL(DECENTRALAND_URL)
@@ -79,75 +72,4 @@ export function placesWithUserCount(
       user_count: hotScenePlaces ? hotScenePlaces.usersTotalCount : 0,
     }
   })
-}
-
-export function createPlaceFromEntityScene(
-  entityScene: EntityScene,
-  data: Partial<Omit<PlaceAttributes, "id">> = {}
-) {
-  const now = Time.from().format("YYYYMMDD hh:mm:ss ZZ")
-  const title = entityScene?.metadata?.display?.title || null
-  const positions = (entityScene?.pointers || []).sort()
-  const tags = (entityScene?.metadata?.tags || [])
-    .slice(0, 100)
-    .map((tag) => tag.slice(0, 25))
-
-  const thumbnail = getThumbnailFromDeployment(entityScene)
-
-  let contact_name = entityScene?.metadata?.contact?.name || null
-  if (contact_name && contact_name.trim() === "author-name") {
-    contact_name = null
-  }
-
-  const placeParsed = {
-    id: uuid(),
-    owner: entityScene?.metadata?.owner || null,
-    title: title ? title.slice(0, 50) : null,
-    description: entityScene?.metadata?.display?.description || null,
-    image: thumbnail,
-    positions,
-    tags,
-    likes: 0,
-    dislikes: 0,
-    favorites: 0,
-    like_rate: 0,
-    base_position: entityScene?.metadata?.scene?.base || positions[0],
-    contact_name,
-    contact_email: entityScene?.metadata?.contact?.email || null,
-    content_rating: entityScene?.metadata?.policy?.contentRating || null,
-    featured: false,
-    disabled: false,
-    disabled_at:
-      !!data.disabled && !data.disabled_at ? now : data.disabled_at || null,
-    created_at: now,
-    updated_at: now,
-    ...data,
-  }
-
-  if (placeParsed.image && !placeParsed.image.startsWith("https")) {
-    placeParsed.image = new URL(placeParsed.image, PLACES_URL).toString()
-  }
-
-  return placeParsed
-}
-
-export async function createEntityScenesFromDefaultPlaces(
-  places: Partial<PlaceAttributes>[]
-) {
-  const batch = places.map((place) => place.base_position!)
-  return Catalyst.get().getEntityScenes(batch)
-}
-
-export async function createPlaceFromDefaultPlaces(
-  places: Partial<PlaceAttributes>[]
-) {
-  const entityScenes = await createEntityScenesFromDefaultPlaces(places)
-  return entityScenes.map((entityScene) =>
-    createPlaceFromEntityScene(
-      entityScene,
-      places.find((place) =>
-        entityScene.pointers.includes(place.base_position!)
-      )
-    )
-  )
 }
--- src/seed/index.test.ts
+++ src/seed/index.test.ts
@@ -1,8 +1,10 @@
 import { validateMigratedPlaces } from "../entities/Place/migration"
-import Places01 from "./01_places.json"
-import Places02 from "./02_places.json"
 
-test("should be able to migrate all default places", async () => {
-  expect(await validateMigratedPlaces(Places01)).toBeTruthy()
-  expect(await validateMigratedPlaces(Places02)).toBeTruthy()
-})
+const files = ["01_places.json", "02_places.json"]
+
+for (const file of files) {
+  test(`should be able to migrate ${file} places`, async () => {
+    const places = await import(`./${file}`)
+    expect(await validateMigratedPlaces(places)).toBeTruthy()
+  })
+}
