75167a45756be9f0a8c71820031625fd16cbfcc5
Braian Mellor <braianj@gmail.com>
2022-12-15 11:36:25 -0300

    fix: print description as markdown in place

--- src/components/Place/PlaceDetails/PlaceDetails.tsx
+++ src/components/Place/PlaceDetails/PlaceDetails.tsx
@@ -1,10 +1,14 @@
 import React, { useMemo, useState } from "react"
 
+import ReactMarkdown from "react-markdown"
+
 import useAsyncMemo from "decentraland-gatsby/dist/hooks/useAsyncMemo"
 import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
 import TokenList from "decentraland-gatsby/dist/utils/dom/TokenList"
 import { Tabs } from "decentraland-ui/dist/components/Tabs/Tabs"
 import { intersects, sum } from "radash/dist/array"
+import rehypeRaw from "rehype-raw"
+import remarkGfm from "remark-gfm"
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon"
 import Label from "semantic-ui-react/dist/commonjs/elements/Label"
 
@@ -108,7 +112,13 @@ export default React.memo(function PlaceDetails(props: PlaceDetailsProps) {
               <>
                 <h3>{l("components.place_detail.description")}</h3>
                 <div>
-                  <p>{place?.description}</p>
+                  <p>
+                    <ReactMarkdown
+                      children={place?.description}
+                      rehypePlugins={[rehypeRaw]}
+                      remarkPlugins={[remarkGfm]}
+                    />
+                  </p>
                 </div>
               </>
             )}
