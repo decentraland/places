f3b9149033a135a313335b9689bcacc0259debfa
Braian Mellor <braianj@gmail.com>
2023-03-29 16:24:45 -0300

    feat: remove Exodus and Antrom RPG from featured

---
+++ src/migrations/1680117659843_automatic-update-static-places.ts
@@ -0,0 +1,31 @@
+import { createPlaceMigration } from "../entities/Place/migration"
+import { PlaceAttributes } from "../entities/Place/types"
+import defaultPlace from "../seed/26_places.json"
+
+const attributes: Array<keyof PlaceAttributes> = [
+  "id", 
+  "title", 
+  "description", 
+  "image", 
+  "highlighted_image", 
+  "featured_image", 
+  "owner", 
+  "tags", 
+  "positions", 
+  "base_position", 
+  "contact_name", 
+  "contact_email", 
+  "content_rating", 
+  "likes", 
+  "dislikes", 
+  "favorites", 
+  "like_rate", 
+  "highlighted", 
+  "featured", 
+  "disabled", 
+  "disabled_at", 
+  "created_at", 
+  "updated_at", 
+]
+
+export const { up, down } = createPlaceMigration(defaultPlace, attributes)
--- src/seed/index.test.ts
+++ src/seed/index.test.ts
@@ -1,32 +1,6 @@
 import { validateMigratedPlaces } from "../entities/Place/migration"
 
-const files = [
-  "01_places.json",
-  "02_places.json",
-  "03_places.json",
-  "04_places.json",
-  "05_places.json",
-  "06_places.json",
-  "07_places.json",
-  "08_places.json",
-  "09_places.json",
-  "10_places.json",
-  "11_places.json",
-  "12_places.json",
-  "13_places.json",
-  "14_places.json",
-  "15_places.json",
-  "16_places.json",
-  "17_places.json",
-  "18_places.json",
-  "19_places.json",
-  "20_places.json",
-  "21_places.json",
-  "22_places.json",
-  "23_places.json",
-  "24_places.json",
-  "25_places.json",
-]
+const files = ["01_places.json", "02_places.json", "03_places.json", "04_places.json", "05_places.json", "06_places.json", "07_places.json", "08_places.json", "09_places.json", "10_places.json", "11_places.json", "12_places.json", "13_places.json", "14_places.json", "15_places.json", "16_places.json", "17_places.json", "18_places.json", "19_places.json", "20_places.json", "21_places.json", "22_places.json", "23_places.json", "24_places.json", "25_places.json", "26_places.json"]
 
 for (const file of files) {
   test(`should be able to migrate ${file} places`, async () => {
