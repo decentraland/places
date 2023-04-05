9af126d83340a440e9bfbaed8f408ed312b800f4
louiseDecentraland <95619478+louiseDecentraland@users.noreply.github.com>
2023-04-05 17:42:35 +0100

    feat: removing fashion week features and replacing with new ones

---
+++ src/migrations/1680712434879_automatic-update-static-places.ts
@@ -0,0 +1,24 @@
+import { createPlaceMigration } from "../entities/Place/migration"
+import { PlaceAttributes } from "../entities/Place/types"
+import defaultPlace from "../seed/30_places.json"
+
+const attributes: Array<keyof PlaceAttributes> = [
+  "title",
+  "description",
+  "image",
+  "highlighted_image",
+  "featured_image",
+  "owner",
+  "tags",
+  "positions",
+  "base_position",
+  "contact_name",
+  "contact_email",
+  "content_rating",
+  "highlighted",
+  "featured",
+  "disabled",
+  "disabled_at",
+]
+
+export const { up, down } = createPlaceMigration(defaultPlace, attributes)
