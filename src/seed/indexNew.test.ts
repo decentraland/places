12d0d8fac4a97f46d6757b2b9091a4c4e73ad9fa
Braian Mellor <braianj@gmail.com>
2023-05-04 17:29:25 -0300

    fix: wrong validation

--- src/seed/indexNew.test.ts
+++ src/seed/indexNew.test.ts
@@ -5,6 +5,6 @@ const files = ["31_places_new.json"]
 for (const file of files) {
   test(`should be able to migrate ${file} places`, async () => {
     const places = await import(`./${file}`)
-    expect(await validatePlacesWorlds(places)).toBeTruthy()
+    expect(await validatePlacesWorlds(places)).toBeUndefined()
   })
 }
