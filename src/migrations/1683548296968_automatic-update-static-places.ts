a36232d4c97cb36fe9f81417c9bd69cc9ab3b768
Braian Mellor <braianj@gmail.com>
2023-05-08 09:24:06 -0300

    feat: rollback last test changes

---
+++ src/migrations/1683548296968_automatic-update-static-places.ts
@@ -0,0 +1,17 @@
+import { createPlaceNewMigrationUpdate } from "../entities/Place/migration"
+import { PlaceAttributes } from "../entities/Place/types"
+import defaultPlace from "../seed/32_places_new.json"
+
+const attributes: Array<keyof PlaceAttributes> = [
+  "base_position",
+  "highlighted_image",
+  "highlighted",
+  "featured",
+  "featured_image",
+  "world_name",
+]
+
+export const { up, down } = createPlaceNewMigrationUpdate(
+  defaultPlace,
+  attributes
+)
--- src/seed/indexNew.test.ts
+++ src/seed/indexNew.test.ts
@@ -1,10 +1,10 @@
 import { validatePlacesWorlds } from "../entities/Place/migration"
 
-const files = ["31_places_new.json"]
+const files = ["31_places_new.json", "32_places_new.json"]
 
 for (const file of files) {
   test(`should be able to migrate ${file} places`, async () => {
     const places = await import(`./${file}`)
-    expect(await validatePlacesWorlds(places)).toBeUndefined()
+    expect(await validatePlacesWorlds(places.update)).toBeUndefined()
   })
 }
