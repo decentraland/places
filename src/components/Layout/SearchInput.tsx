8be5b9e0c54f4a7b259efaa0c3ed2729e6a4251e
jmoguilevsky <jmoguilevsky@gmail.com>
2023-07-21 12:08:33 -0300

    chore: add debounce to search

--- src/components/Layout/SearchInput.tsx
+++ src/components/Layout/SearchInput.tsx
@@ -1,17 +1,26 @@
-import React from "react"
+import React, { useMemo } from "react"
 
 import TokenList from "decentraland-gatsby/dist/utils/dom/TokenList"
+import debounce from "decentraland-gatsby/dist/utils/function/debounce"
 
 import "./SearchInput.css"
 
 export default function SearchInput(
   props: React.InputHTMLAttributes<HTMLInputElement>
 ) {
+  const { onChange } = props
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange!(e)
+  }
+
+  const debouncedHandleChange = useMemo(() => debounce(handleChange, 350), [])
+
   return (
     <input
       {...props}
       placeholder={props.placeholder || "Search..."}
       className={TokenList.join(["search__input", props.className])}
+      onChange={debouncedHandleChange}
     />
   )
 }
