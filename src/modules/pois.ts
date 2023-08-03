67f9faecb76d650db966b0200872b5ce37383010
Braian Mellor <braianj@gmail.com>
2023-08-03 11:05:42 -0300

    feat: get pois from rpc

--- src/modules/pois.ts
+++ src/modules/pois.ts
@@ -1,4 +1,32 @@
-import { cache } from "dcl-catalyst-client/dist/contracts-snapshots/data"
+import { l2Contracts } from "@dcl/catalyst-contracts/dist"
+import { listAbi } from "@dcl/catalyst-contracts/dist/abis"
+import {
+  PoiContract,
+  getPoisFromContract,
+} from "@dcl/catalyst-contracts/dist/utils"
+import RequestManager, { ContractFactory, HTTPProvider } from "eth-connect"
 import { memo } from "radash/dist/curry"
 
-export const getPois = memo(async () => cache.pois.polygon, { ttl: Infinity })
+async function createContract(
+  address: string,
+  provider: HTTPProvider
+): Promise<PoiContract> {
+  const requestManager = new RequestManager(provider)
+  const factory = new ContractFactory(requestManager, listAbi)
+  return (await factory.at(address)) as any
+}
+
+export const getPois = memo(
+  async () => {
+    const opts = { fetch: fetch as any }
+
+    const polygon = new HTTPProvider(
+      "https://rpc.decentraland.org/polygon?project=catalyst-contracts-ci",
+      opts
+    )
+    const contract = await createContract(l2Contracts.polygon.poi, polygon)
+    const pois = await getPoisFromContract(contract)
+    return pois
+  },
+  { ttl: Infinity }
+)
