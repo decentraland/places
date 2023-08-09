0f2e0562693827f0bca3202b0107f306e4914fd4
Braian Mellor <braianj@gmail.com>
2023-08-09 11:01:30 -0300

    feat: update getPois method

--- src/modules/pois.ts
+++ src/modules/pois.ts
@@ -1,4 +1,22 @@
-import { cache } from "dcl-catalyst-client/dist/contracts-snapshots/data"
+import Time from "decentraland-gatsby/dist/utils/date/Time"
+import env from "decentraland-gatsby/dist/utils/env"
 import { memo } from "radash/dist/curry"
 
-export const getPois = memo(async () => cache.pois.polygon, { ttl: Infinity })
+const DCL_LIST_URL = env(
+  `GATSBY_DCL_LIST_URL`,
+  `https://dcl-name-stats.decentraland.org`
+)
+
+export const getPois = memo(
+  async (): Promise<string[]> => {
+    try {
+      const poisFetch = await fetch(`${DCL_LIST_URL}/pois`, { method: "POST" })
+      const poisData = await poisFetch.json()
+
+      return poisData.data
+    } catch (error) {
+      return []
+    }
+  },
+  { ttl: Time.Hour }
+)
