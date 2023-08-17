b59745517a880829d47a4d1a9987e2a153b976d6
lauti7 <lautigbustos@gmail.com>
2023-08-17 12:35:57 -0300

    Revert "feat: rename like_score as like_rate and remove old like_rate"
    This reverts commit e36334ca08357eb94acbf21fad71380eb52fd504.

--- src/migrations/1692121352544_change-like-score-field.ts
+++ src/migrations/1692121352544_change-like-score-field.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Type } from "decentraland-gatsby/dist/entities/Database/types"
 import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate"
 
 import PlaceModel from "../entities/Place/model"
@@ -15,27 +14,9 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createIndex(PlaceModel.tableName, ["like_score", "deployed_at"], {
     where: "disabled is false",
   })
-
-  pgm.dropColumn(PlaceModel.tableName, "like_rate")
-
-  pgm.renameColumn(PlaceModel.tableName, "like_score", "like_rate")
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.renameColumn(PlaceModel.tableName, "like_rate", "like_score")
-
-  pgm.addColumn(PlaceModel.tableName, {
-    like_rate: {
-      type: Type.Real,
-      notNull: true,
-      default: 0,
-    },
-  })
-
-  pgm.createIndex(PlaceModel.tableName, ["like_rate"], {
-    where: "disabled is false and world is false",
-  })
-
   pgm.alterColumn(PlaceModel.tableName, "like_score", {
     notNull: true,
     default: 0,
