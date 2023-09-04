42e83f9a3b621d01f619a7427816b3a5993d81e2
Florencia Barreto <florbarrlad@gmail.com>
2023-09-04 19:23:04 +0200

    feat: added new user menu

--- src/components/Layout/UserMenu.tsx
+++ src/components/Layout/UserMenu.tsx
@@ -1,19 +1,26 @@
 import React from "react"
 
 import Menu from "decentraland-gatsby/dist/components/User/UserMenu"
+import UserInformation from "decentraland-gatsby/dist/components/User/UserInformation"
 import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
+import useFeatureFlagContext from "decentraland-gatsby/dist/context/FeatureFlag/useFeatureFlagContext"
 import { navigate } from "decentraland-gatsby/dist/plugins/intl"
 import MenuItem from "semantic-ui-react/dist/commonjs/collections/Menu/MenuItem"
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon/Icon"
 
 import locations from "../../modules/locations"
+import { FeatureFlags } from "../../modules/ff"
 
 const handleClickDocs = () => navigate(locations.docs())
 
 export default React.memo(function UserMenu() {
   const l = useFormatMessage()
 
-  return (
+  const [ff] = useFeatureFlagContext()
+
+  return ff.flags[FeatureFlags.NewNavbar] ? (
+    <UserInformation />
+  ) : (
     <Menu
       menuItems={
         <>
--- src/modules/ff.ts
+++ src/modules/ff.ts
@@ -2,4 +2,5 @@ export enum FeatureFlags {
   Maintenance = "places-maintenance",
   HideWorlds = "places-hide-worlds",
   HideWorldActiveUser = "places-hide-world-active-user",
+  NewNavbar = "places-new-navbar-dropdown",
 }
