bcaba2486d0f724a324b49221363350a8c1eabdf
Florencia Barreto <florbarrlad@gmail.com>
2023-09-05 19:55:01 +0200

    fix: eslint

--- src/components/Layout/UserMenu.tsx
+++ src/components/Layout/UserMenu.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 
-import Menu from "decentraland-gatsby/dist/components/User/UserMenu"
 import UserInformation from "decentraland-gatsby/dist/components/User/UserInformation"
-import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
+import Menu from "decentraland-gatsby/dist/components/User/UserMenu"
 import useFeatureFlagContext from "decentraland-gatsby/dist/context/FeatureFlag/useFeatureFlagContext"
+import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
 import { navigate } from "decentraland-gatsby/dist/plugins/intl"
 import MenuItem from "semantic-ui-react/dist/commonjs/collections/Menu/MenuItem"
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon/Icon"
 
-import locations from "../../modules/locations"
 import { FeatureFlags } from "../../modules/ff"
+import locations from "../../modules/locations"
 
 const handleClickDocs = () => navigate(locations.docs())
 
