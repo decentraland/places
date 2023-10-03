329c11546f3d95b8167003a55c40ed79c321dd06
Braian Mellor <braianj@gmail.com>
2023-10-03 15:25:38 -0300

    feat: RatingButton component

---
+++ src/components/Button/RatingButton.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
+import { SceneContentRating } from "decentraland-gatsby/dist/utils/api/Catalyst.types"
+import TokenList from "decentraland-gatsby/dist/utils/dom/TokenList"
+
+import "./RatingButton.css"
+
+export type RatingButtonProps = {
+  onClick: (e: React.MouseEvent<HTMLDivElement>) => void
+  rating: SceneContentRating
+  active: boolean
+  current: boolean
+}
+
+export default React.memo(function RatingButton(props: RatingButtonProps) {
+  const { onClick, current, active } = props
+  const l = useFormatMessage()
+  const rating = props.rating.toLowerCase()
+  return (
+    <div
+      className={TokenList.join([
+        "rating-button",
+        `rating-${rating}`,
+        current && "current",
+        active && "selected",
+      ])}
+      onClick={onClick}
+    >
+      <h1 className="rating-label">
+        {l(`components.rating_button.rating_${rating}`)}
+      </h1>
+      <label className="rating-label">
+        {l(`components.rating_button.rating_${rating}_label`)}
+      </label>
+    </div>
+  )
+})
