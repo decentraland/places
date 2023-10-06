d92a855933706ac08e25771e7424b26e542fe6d8
lauti7 <lautigbustos@gmail.com>
2023-10-06 11:25:03 -0300

    fix: remove useless constraints

--- src/migrations/1695656505396_place-category-pivot-table.ts
+++ src/migrations/1695656505396_place-category-pivot-table.ts
@@ -2,8 +2,6 @@
 import { Type } from "decentraland-gatsby/dist/entities/Database/types"
 import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate"
 
-import CategoryModel from "../entities/Category/model"
-import PlaceModel from "../entities/Place/model"
 import PlaceCategories from "../entities/PlaceCategories/model"
 
 export const shorthands: ColumnDefinitions | undefined = undefined
@@ -17,20 +15,6 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     category_id: {
       type: Type.Varchar(50),
       notNull: true,
-    },
-  })
-
-  pgm.addConstraint(PlaceCategories.tableName, "place_id_fk", {
-    foreignKeys: {
-      columns: "place_id",
-      references: `${PlaceModel.tableName}(id)`,
-    },
-  })
-
-  pgm.addConstraint(PlaceCategories.tableName, "category_id_fk", {
-    foreignKeys: {
-      columns: "category_id",
-      references: `${CategoryModel.tableName}(name)`,
     },
   })
 
