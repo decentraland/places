e011cdee1eaa013e2060e9a84be25b899154df66
lauti7 <lautigbustos@gmail.com>
2023-10-08 11:39:33 -0300

    fix: use ui Modal

--- src/components/Categories/CategoriesModal.tsx
+++ src/components/Categories/CategoriesModal.tsx
@@ -1,92 +1,117 @@
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 
 import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
 import { Button } from "decentraland-ui/dist/components/Button/Button"
+import { Modal, ModalProps } from "decentraland-ui/dist/components/Modal/Modal"
 
-import { CategoryFilter } from "./CategoryFilter"
+import { Check } from "../Icon/Check"
+import { Close } from "../Icon/Close"
+import { CategoriesFilters } from "./CategoriesFilters"
 
 import "./CategoriesModal.css"
 
-type CategoriesModalProps = {
-  categories: { categoryId: string; active: boolean }[]
+type CategoriesModalProps = ModalProps & {
+  categories: { name: string; active: boolean; count: number }[]
   onClose: () => void
   onClearAll: () => void
-  onApplySelection: (categoryIds: string[]) => void
+  onApplySelection: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    names: string[]
+  ) => void
 }
 
-export const CategoriesModal = ({
-  categories,
-  onClearAll,
-  onClose,
-  onApplySelection,
-}: CategoriesModalProps) => {
-  const l = useFormatMessage()
+export const CategoriesModal = React.memo(
+  ({
+    categories,
+    onClearAll,
+    onClose,
+    onApplySelection,
+    ...rest
+  }: CategoriesModalProps) => {
+    const l = useFormatMessage()
 
-  const [selectedCategories, setSelectedCategories] = useState(() => {
-    return categories
-      .filter(({ active }) => active)
-      .map(({ categoryId }) => categoryId)
-  })
+    const [selectedCategories, setSelectedCategories] = useState(() => {
+      return new Set(
+        categories.filter(({ active }) => active).map(({ name }) => name)
+      )
+    })
 
-  useEffect(() => {
-    console.log("rendering due to categories")
-    console.log(selectedCategories)
-  }, [categories])
+    useEffect(() => {
+      setSelectedCategories(() => {
+        return new Set(
+          categories.filter(({ active }) => active).map(({ name }) => name)
+        )
+      })
+    }, [categories])
 
-  return (
-    <div className="categories-modal__layer">
-      <div className="categories-modal__box">
-        <div className="categories-modal__header">
+    const onFilterChange = useCallback(
+      (categories: { active: boolean; name: string }[]) => {
+        const newSet = new Set(selectedCategories)
+        for (const category of categories) {
+          if (category.active) {
+            newSet.add(category.name)
+          } else {
+            newSet.delete(category.name)
+          }
+        }
+        setSelectedCategories(newSet)
+      },
+      []
+    )
+
+    return (
+      <Modal {...rest} className="categories-modal__box">
+        <Modal.Header className="categories-modal__header">
           <Button
             basic
             content="Clear All"
-            onClick={() => onClearAll()}
-            disabled={!!categories.length}
+            onClick={onClearAll}
+            disabled={!!selectedCategories.size}
+            className="clear-all"
           />
-          <p>
-            {selectedCategories.length
-              ? `${selectedCategories.length} ${l(
+          <p
+            className={
+              selectedCategories.size ? "categories-modal__text--smaller" : ""
+            }
+          >
+            {selectedCategories.size
+              ? `${selectedCategories.size} ${l(
                   "categories.modal.category_selected"
                 )}`
               : l("categories.modal.one_or_more")}
           </p>
           <Button
+            className="close-btn"
             basic
-            content="Close"
-            onClick={() => {
-              onClose()
-            }}
+            content={<Close width="24" height="24" type="secondary" />}
+            onClick={onClose}
           />
-        </div>
-        <div className="categories-modal__content">
-          {categories.map(({ categoryId }) => (
-            <CategoryFilter
-              key={categoryId}
-              category={categoryId}
-              active={selectedCategories.includes(categoryId)}
-              onAddFilter={() => {
-                if (!selectedCategories.includes(categoryId)) {
-                  setSelectedCategories([...selectedCategories, categoryId])
+        </Modal.Header>
+        <Modal.Content className="categories-modal__content">
+          {
+            <CategoriesFilters
+              categories={categories.map((category) => {
+                const withActiveUpdated = {
+                  ...category,
+                  active: selectedCategories.has(category.name),
                 }
-              }}
-              onRemoveFilter={() => {
-                setSelectedCategories((selected) =>
-                  selected.filter((c) => c != categoryId)
-                )
-              }}
-              useCheck
+                return withActiveUpdated
+              })}
+              onChange={onFilterChange}
+              filtersIcon={<Check width="20" height="20" />}
             />
-          ))}
-        </div>
-        <div className="categories-modal__footer">
+          }
+        </Modal.Content>
+        <Modal.Actions className="categories-modal__footer">
           <Button
             primary
             size="medium"
             content="Apply Selection"
-            onClick={() => onApplySelection([])}
+            onClick={(e) => onApplySelection(e, [...selectedCategories])}
+            disabled={selectedCategories.size === 0}
           />
-        </div>
-      </div>
-    </div>
-  )
-}
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+)
