d51b9382c8c2b2a654094725e1c91bf18a3fe64b
lauti7 <lautigbustos@gmail.com>
2023-10-08 11:37:57 -0300

    fix: improve CategoryFilter

--- src/components/Categories/CategoryFilter.tsx
+++ src/components/Categories/CategoryFilter.tsx
@@ -3,25 +3,23 @@ import React from "react"
 import useFormatMessage from "decentraland-gatsby/dist/hooks/useFormatMessage"
 import { Filter } from "decentraland-ui/dist/components/Filter/Filter"
 
-import { Check } from "../Icon/Check"
-import { Close } from "../Icon/Close"
-
 import "./CategoryFilter.css"
 
 type CategoryFilterProps = {
   category: string
+  onChange?: (
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+    props: { active: boolean; category: string }
+  ) => void
   active?: boolean
-  onRemoveFilter?: () => void
-  onAddFilter?: () => void
-  useCheck?: boolean
+  actionIcon?: JSX.Element
 }
 
 export const CategoryFilter = ({
   category,
-  onRemoveFilter,
-  onAddFilter,
   active,
-  useCheck,
+  onChange,
+  actionIcon,
 }: CategoryFilterProps) => {
   const l = useFormatMessage()
 
@@ -30,22 +28,11 @@ export const CategoryFilter = ({
       className={`category-filter__box ${
         !active ? "category-filter__box--not-active" : ""
       }`}
-      onClick={() => onAddFilter && onAddFilter()}
+      onClick={(e) => onChange && onChange(e, { active: !active, category })}
     >
       <Filter>
         {l(`categories.${category}`)}
-        {active && (
-          <span
-            className="category-filter__remove-btn"
-            onClick={onRemoveFilter ? () => onRemoveFilter() : undefined}
-          >
-            {useCheck ? (
-              <Check width="20" height="20" />
-            ) : (
-              <Close width="20" height="20" />
-            )}
-          </span>
-        )}
+        {active && <span className="category-filter__icon">{actionIcon}</span>}
       </Filter>
     </span>
   )
