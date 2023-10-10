cd3bcc8f7d96ad275edda7c8459388119ef08985
Hugo Arregui <hugo@decentraland.org>
2023-10-10 14:26:01 -0300

    use realm provider to fetch hot scenes data

--- src/modules/hotScenes.ts
+++ src/modules/hotScenes.ts
@@ -1,11 +1,15 @@
-import Catalyst from "decentraland-gatsby/dist/utils/api/Catalyst"
 import Time from "decentraland-gatsby/dist/utils/date/Time"
 import { memo } from "radash/dist/curry"
 
 export const getHotScenes = memo(
   async () => {
     try {
-      return await Catalyst.getInstance().getHostScenes()
+      const hotScenesFetch = await fetch(
+        "https://realm-provider.decentraland.org/hot-scenes"
+      )
+      const hotScenesData = await hotScenesFetch.json()
+
+      return hotScenesData
     } catch (error) {
       return []
     }
