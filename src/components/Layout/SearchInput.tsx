74b4549dca5341472b33b73efd5c766c6c81365b
lauti7 <lautigbustos@gmail.com>
2023-10-20 14:30:10 -0300

    fix: buggy handleChange function

--- src/components/Layout/SearchInput.tsx
+++ src/components/Layout/SearchInput.tsx
@@ -1,28 +1,28 @@
-import React, { useCallback, useMemo } from "react"
+import React, { useMemo } from "react"
 
 import TokenList from "decentraland-gatsby/dist/utils/dom/TokenList"
 import debounce from "decentraland-gatsby/dist/utils/function/debounce"
 
 import "./SearchInput.css"
 
-export default function SearchInput(
-  props: React.InputHTMLAttributes<HTMLInputElement>
-) {
-  const { onChange } = props
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      onChange!(e)
-    },
-    [onChange]
-  )
+export default function SearchInput({
+  onChange,
+  ...rest
+}: React.InputHTMLAttributes<HTMLInputElement>) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange!(e)
+  }
 
-  const debouncedHandleChange = useMemo(() => debounce(handleChange, 350), [])
+  const debouncedHandleChange = useMemo(
+    () => debounce(handleChange, 350),
+    [handleChange]
+  )
 
   return (
     <input
-      {...props}
-      placeholder={props.placeholder || "Search..."}
-      className={TokenList.join(["search__input", props.className])}
+      {...rest}
+      placeholder={rest.placeholder || "Search..."}
+      className={TokenList.join(["search__input", rest.className])}
       onChange={debouncedHandleChange}
     />
   )
