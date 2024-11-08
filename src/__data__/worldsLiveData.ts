c172c5c1022875fb3045d3074d4456d13adaf400
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-11-08 18:58:42 -0300

    feat: Add tests

---
+++ src/__data__/worldsLiveData.ts
@@ -0,0 +1,6 @@
+import { WorldLiveDataProps } from "../entities/World/types"
+
+export const worldsLiveData: WorldLiveDataProps = {
+  perWorld: [{ worldName: "test.dcl.eth", users: 30 }],
+  totalUsers: 30,
+}
--- src/entities/Map/routes/getAllPlacesList.test.ts
+++ src/entities/Map/routes/getAllPlacesList.test.ts
@@ -1,70 +1,79 @@
 import { Request } from "decentraland-gatsby/dist/entities/Route/wkc/request/Request"
 
+import { allPlacesWithAggregatedAttributes } from "../../../__data__/allPlacesWithAggregatedAttributes"
 import { hotSceneGenesisPlaza } from "../../../__data__/hotSceneGenesisPlaza"
-import { placeGenesisPlazaWithAggregatedAttributes } from "../../../__data__/placeGenesisPlazaWithAggregatedAttributes"
 import { sceneStatsGenesisPlaza } from "../../../__data__/sceneStatsGenesisPlaza"
+import { worldsLiveData } from "../../../__data__/worldsLiveData"
 import PlaceModel from "../../Place/model"
 import * as hotScenes from "../../RealmProvider/utils"
 import * as sceneStats from "../../SceneStats/utils"
-import { getMapPlaces } from "./getMapPlaces"
+import * as worldsUtils from "../../World/utils"
+import { getAllPlacesList } from "./getAllPlacesList"
 
 const find = jest.spyOn(PlaceModel, "namedQuery")
 const catalystHotScenes = jest.spyOn(hotScenes, "getHotScenes")
 const catalystSceneStats = jest.spyOn(sceneStats, "getSceneStats")
+const worldsContentServerLiveData = jest.spyOn(worldsUtils, "getWorldsLiveData")
 
 afterEach(() => {
   find.mockReset()
   catalystHotScenes.mockReset()
   catalystSceneStats.mockReset()
+  worldsContentServerLiveData.mockReset()
 })
 
-test("should return a object of places with no query", async () => {
-  find.mockResolvedValueOnce(
-    Promise.resolve([placeGenesisPlazaWithAggregatedAttributes])
-  )
-  find.mockResolvedValueOnce(Promise.resolve([{ total: 1 }]))
+test("should return a list of places with no query", async () => {
+  find.mockResolvedValueOnce(Promise.resolve(allPlacesWithAggregatedAttributes))
+  find.mockResolvedValueOnce(Promise.resolve([{ total: 2 }]))
   catalystHotScenes.mockReturnValueOnce([hotSceneGenesisPlaza])
   catalystSceneStats.mockResolvedValueOnce(
     Promise.resolve(sceneStatsGenesisPlaza)
   )
+  worldsContentServerLiveData.mockReturnValueOnce(worldsLiveData)
   const request = new Request("/")
   const url = new URL("https://localhost/")
-  const placeResponse = await getMapPlaces({
+  const placeResponse = await getAllPlacesList({
     request,
     url,
   })
   expect(placeResponse.body).toEqual({
     ok: true,
-    total: 1,
-    data: {
-      ["-9,-9"]: {
-        ...placeGenesisPlazaWithAggregatedAttributes,
+    total: 2,
+    data: [
+      {
+        ...allPlacesWithAggregatedAttributes[0],
         user_count: hotSceneGenesisPlaza.usersTotalCount,
         user_visits: sceneStatsGenesisPlaza["-9,-9"].last_30d.users,
-        positions: undefined,
       },
-    },
+      {
+        ...allPlacesWithAggregatedAttributes[1],
+        user_count: worldsLiveData.totalUsers,
+        // TODO: Get user visits from world stats
+        user_visits: 0,
+      },
+    ],
   })
   expect(find.mock.calls.length).toBe(2)
   expect(catalystHotScenes.mock.calls.length).toBe(1)
   expect(catalystSceneStats.mock.calls.length).toBe(1)
+  expect(worldsContentServerLiveData.mock.calls.length).toBe(1)
 })
 
-test("should return a object of places with query", async () => {
+test("should return a list of places with query", async () => {
   find.mockResolvedValueOnce(
-    Promise.resolve([placeGenesisPlazaWithAggregatedAttributes])
+    Promise.resolve(allPlacesWithAggregatedAttributes.slice(0, 1))
   )
   find.mockResolvedValueOnce(Promise.resolve([{ total: 1 }]))
   catalystHotScenes.mockReturnValueOnce([hotSceneGenesisPlaza])
-
   catalystSceneStats.mockResolvedValueOnce(
     Promise.resolve(sceneStatsGenesisPlaza)
   )
+  worldsContentServerLiveData.mockReturnValueOnce(worldsLiveData)
   const request = new Request("/")
   const url = new URL(
     "https://localhost/?position=-9,-9&limit=1&offset=1&order_by=like_rate&order=asc"
   )
-  const placeResponse = await getMapPlaces({
+  const placeResponse = await getAllPlacesList({
     request,
     url,
   })
@@ -72,91 +81,99 @@ test("should return a object of places with query", async () => {
   expect(placeResponse.body).toEqual({
     ok: true,
     total: 1,
-    data: {
-      ["-9,-9"]: {
-        ...placeGenesisPlazaWithAggregatedAttributes,
+    data: [
+      {
+        ...allPlacesWithAggregatedAttributes[0],
         user_count: hotSceneGenesisPlaza.usersTotalCount,
         user_visits: sceneStatsGenesisPlaza["-9,-9"].last_30d.users,
-        positions: undefined,
       },
-    },
+    ],
   })
   expect(find.mock.calls.length).toBe(2)
   expect(catalystHotScenes.mock.calls.length).toBe(1)
   expect(catalystSceneStats.mock.calls.length).toBe(1)
+  expect(worldsContentServerLiveData.mock.calls.length).toBe(1)
 })
 
-test("should return a object of places with order by most_active", async () => {
-  find.mockResolvedValueOnce(
-    Promise.resolve([placeGenesisPlazaWithAggregatedAttributes])
-  )
+test("should return a list of places with order by most_active", async () => {
+  find.mockResolvedValueOnce(Promise.resolve(allPlacesWithAggregatedAttributes))
   catalystHotScenes.mockReturnValueOnce([hotSceneGenesisPlaza])
-
   catalystSceneStats.mockResolvedValueOnce(
     Promise.resolve(sceneStatsGenesisPlaza)
   )
+  worldsContentServerLiveData.mockReturnValueOnce(worldsLiveData)
   const request = new Request("/")
-  const url = new URL("https://localhost/?&order_by=most_active&limit=1")
-  const placeResponse = await getMapPlaces({
+  const url = new URL("https://localhost/?&order_by=most_active")
+  const placeResponse = await getAllPlacesList({
     request,
     url,
   })
 
   expect(placeResponse.body).toEqual({
     ok: true,
-    total: 1,
-    data: {
-      ["-9,-9"]: {
-        ...placeGenesisPlazaWithAggregatedAttributes,
+    total: 2,
+    data: [
+      {
+        ...allPlacesWithAggregatedAttributes[1],
+        user_count: worldsLiveData.totalUsers,
+        // TODO: Get user visits from world stats
+        user_visits: 0,
+      },
+      {
+        ...allPlacesWithAggregatedAttributes[0],
         user_count: hotSceneGenesisPlaza.usersTotalCount,
         user_visits: sceneStatsGenesisPlaza["-9,-9"].last_30d.users,
-        positions: undefined,
       },
-    },
+    ],
   })
   expect(find.mock.calls.length).toBe(1)
   expect(catalystHotScenes.mock.calls.length).toBe(1)
   expect(catalystSceneStats.mock.calls.length).toBe(1)
+  expect(worldsContentServerLiveData.mock.calls.length).toBe(1)
 })
 
-test("should return a object of places with Realm details", async () => {
-  find.mockResolvedValueOnce(
-    Promise.resolve([placeGenesisPlazaWithAggregatedAttributes])
-  )
-  find.mockResolvedValueOnce(Promise.resolve([{ total: 1 }]))
+test("should return a list of places with Realm details", async () => {
+  find.mockResolvedValueOnce(Promise.resolve(allPlacesWithAggregatedAttributes))
+  find.mockResolvedValueOnce(Promise.resolve([{ total: 2 }]))
   catalystHotScenes.mockReturnValueOnce([hotSceneGenesisPlaza])
-
   catalystSceneStats.mockResolvedValueOnce(
     Promise.resolve(sceneStatsGenesisPlaza)
   )
+  worldsContentServerLiveData.mockReturnValueOnce(worldsLiveData)
   const request = new Request("/")
   const url = new URL("https://localhost/?with_realms_detail=true")
-  const placeResponse = await getMapPlaces({
+  const placeResponse = await getAllPlacesList({
     request,
     url,
   })
   expect(placeResponse.body).toEqual({
     ok: true,
-    total: 1,
-    data: {
-      ["-9,-9"]: {
-        ...placeGenesisPlazaWithAggregatedAttributes,
+    total: 2,
+    data: [
+      {
+        ...allPlacesWithAggregatedAttributes[0],
         user_count: hotSceneGenesisPlaza.usersTotalCount,
         user_visits: sceneStatsGenesisPlaza["-9,-9"].last_30d.users,
         realms_detail: hotSceneGenesisPlaza.realms,
-        positions: undefined,
       },
-    },
+      {
+        ...allPlacesWithAggregatedAttributes[1],
+        user_count: worldsLiveData.totalUsers,
+        // TODO: Get user visits from world stats
+        user_visits: 0,
+      },
+    ],
   })
   expect(find.mock.calls.length).toBe(2)
   expect(catalystHotScenes.mock.calls.length).toBe(1)
   expect(catalystSceneStats.mock.calls.length).toBe(1)
+  expect(worldsContentServerLiveData.mock.calls.length).toBe(1)
 })
 
 test("should return 0 as total list when query onlyFavorites with no auth", async () => {
   const request = new Request("/")
   const url = new URL("https://localhost/?only_favorites=true")
-  const placeResponse = await getMapPlaces({
+  const placeResponse = await getAllPlacesList({
     request,
     url,
   })
@@ -174,7 +191,7 @@ test("should return an error when a wrong value has been sent in the query", asy
   const url = new URL("https://localhost/?order_by=fake")
 
   expect(async () =>
-    getMapPlaces({
+    getAllPlacesList({
       request,
       url,
     })
