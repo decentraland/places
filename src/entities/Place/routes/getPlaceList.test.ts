2b1d0820c8558fe243f98036f2c8348af5909c1f
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-12-17 11:23:45 -0300

    fix: Tests

--- src/entities/Place/routes/getPlaceList.test.ts
+++ src/entities/Place/routes/getPlaceList.test.ts
@@ -3,18 +3,19 @@ import { Request } from "decentraland-gatsby/dist/entities/Route/wkc/request/Req
 import { hotSceneGenesisPlaza } from "../../../__data__/hotSceneGenesisPlaza"
 import { placeGenesisPlazaWithAggregatedAttributes } from "../../../__data__/placeGenesisPlazaWithAggregatedAttributes"
 import { sceneStatsGenesisPlaza } from "../../../__data__/sceneStatsGenesisPlaza"
-import DataTeam from "../../../api/DataTeam"
-import * as hotScenesModule from "../../../modules/hotScenes"
+import * as sceneStatsModule from "../../SceneStats/utils"
+import * as hotScenesModule from "../../RealmProvider/utils"
 import PlaceModel from "../model"
 import { getPlaceList } from "./getPlaceList"
 
 const find = jest.spyOn(PlaceModel, "namedQuery")
 const catalystHotScenes = jest.spyOn(hotScenesModule, "getHotScenes")
-const catalystSceneStats = jest.spyOn(DataTeam.get(), "getSceneStats")
+const catalystSceneStats = jest.spyOn(sceneStatsModule, "getSceneStats")
 
 afterEach(() => {
   find.mockReset()
   catalystHotScenes.mockReset()
+  catalystSceneStats.mockReset()
 })
 
 test("should return a list of places with no query", async () => {
