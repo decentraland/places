c450b85f89f5565960e05b9059ac4c66089cc494
Gabriel Díaz <git@subsme.slmail.me>
2024-12-20 14:35:11 -0300

    Revert "fix: Most active Places Sort by (#570)"
    This reverts commit 74aa58a76305bcecc8145ea2384269fbfee2abea.

--- src/entities/RealmProvider/utils.test.ts
+++ src/entities/RealmProvider/utils.test.ts
@@ -1,31 +1,15 @@
 import fetch from "node-fetch"
 
-import {
-  hotSceneGenesisPlaza,
-  hotSceneGenesisPlazaLegacy,
-} from "../../__data__/hotSceneGenesisPlaza"
-import RealmProvider, {
-  fetchHotScenesAndUpdateCache,
-  getHotScenes,
-} from "./utils"
+import { hotSceneGenesisPlaza } from "../../__data__/hotSceneGenesisPlaza"
+import RealmProvider from "./utils"
 
 jest.mock("node-fetch", () => jest.fn())
-jest.mock("decentraland-gatsby/dist/utils/env", () => {
-  return jest.fn((key: string) => {
-    switch (key) {
-      case "REALM_PROVIDER_URL":
-        return "https://realm-provider/"
-      case "ARCHIPELAGO_URL":
-        return "https://archipelago-provider/"
-      default:
-        return "https://default-provider/"
-    }
-  })
-})
+jest.mock("decentraland-gatsby/dist/utils/env", () =>
+  jest.fn(() => "https://realm-provider/")
+)
 
 describe("RealmProvider", () => {
-  const url = "https://archipelago-provider/hot-scenes"
-  const legacyUrl = "https://realm-provider/hot-scenes"
+  const url = "https://realm-provider/hot-scenes"
   const mockFetch = fetch as jest.MockedFunction<typeof fetch>
 
   beforeEach(() => {
@@ -33,27 +17,16 @@ describe("RealmProvider", () => {
   })
 
   it("should fetch hot scenes successfully", async () => {
-    mockFetch
-      .mockResolvedValueOnce({
-        ok: true,
-        json: jest.fn().mockResolvedValueOnce([hotSceneGenesisPlazaLegacy]),
-      } as any)
-      .mockResolvedValueOnce({
-        ok: true,
-        json: jest.fn().mockResolvedValueOnce([hotSceneGenesisPlaza]),
-      } as any)
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(hotSceneGenesisPlaza),
+    } as any)
 
     const realmProvider = RealmProvider.get()
-    const [hotScenesLegacy, hotScenes] = await Promise.all([
-      realmProvider.getHotScenes(true),
-      realmProvider.getHotScenes(),
-    ])
-
-    expect(mockFetch).toHaveBeenCalledWith(legacyUrl, expect.any(Object))
-    expect(hotScenesLegacy[0]).toEqual(hotSceneGenesisPlazaLegacy)
+    const hotScenes = await realmProvider.getHotScenes()
 
     expect(mockFetch).toHaveBeenCalledWith(url, expect.any(Object))
-    expect(hotScenes[0]).toEqual(hotSceneGenesisPlaza)
+    expect(hotScenes).toEqual(hotSceneGenesisPlaza)
   })
 
   it("should handle fetch failure", async () => {
@@ -67,54 +40,5 @@ describe("RealmProvider", () => {
     await expect(realmProvider.getHotScenes()).rejects.toThrow(
       "Failed to fetch hot scenes: Internal Server Error"
     )
-  })
-
-  describe("fetchHotScenesAndUpdateCache", () => {
-    describe("when the scene is in both sources", () => {
-      it("should merge and cache hot scenes from both sources", async () => {
-        mockFetch
-          .mockResolvedValueOnce({
-            ok: true,
-            json: jest.fn().mockResolvedValueOnce([hotSceneGenesisPlazaLegacy]),
-          } as any)
-          .mockResolvedValueOnce({
-            ok: true,
-            json: jest.fn().mockResolvedValueOnce([hotSceneGenesisPlaza]),
-          } as any)
-
-        await fetchHotScenesAndUpdateCache()
-        const result = getHotScenes()
-
-        expect(result).toHaveLength(1)
-        expect(result[0].usersTotalCount).toBe(21)
-      })
-    })
-
-    describe("when the scene is in only one source", () => {
-      it("should keep separate scenes with different coordinates", async () => {
-        const scene = {
-          ...hotSceneGenesisPlaza,
-          baseCoords: [100, 100],
-          usersTotalCount: 1,
-        }
-
-        mockFetch
-          .mockResolvedValueOnce({
-            ok: true,
-            json: jest.fn().mockResolvedValueOnce([hotSceneGenesisPlazaLegacy]),
-          } as any)
-          .mockResolvedValueOnce({
-            ok: true,
-            json: jest.fn().mockResolvedValueOnce([scene]),
-          } as any)
-
-        await fetchHotScenesAndUpdateCache()
-        const result = getHotScenes()
-
-        expect(result).toHaveLength(2)
-        expect(result[0]?.usersTotalCount).toBe(11)
-        expect(result[1]?.usersTotalCount).toBe(1)
-      })
-    })
   })
 })
--- src/entities/RealmProvider/utils.ts
+++ src/entities/RealmProvider/utils.ts
@@ -8,50 +8,40 @@ const DEFAULT_HOST_SCENE = [] as HotScene[]
 
 let memory = DEFAULT_HOST_SCENE
 
-// TODO: Remove urlLegacy reference once the Legacy Explorer is not longer used
 export default class RealmProvider {
   static Url = env(
-    "ARCHIPELAGO_URL",
-    "https://archipelago-ea-stats.decentraland.org/"
-  )
-  static UrlLegacy = env(
     "REALM_PROVIDER_URL",
     "https://realm-provider.decentraland.org/"
   )
   static Cache = new Map<string, RealmProvider>()
 
   private url: string
-  private urlLegacy: string
 
-  constructor(url: string, urlLegacy: string) {
+  constructor(url: string) {
     this.url = url
-    this.urlLegacy = urlLegacy
   }
 
   // Singleton instance based on URL
-  static from(url: string, urlLegacy: string) {
+  static from(url: string) {
     if (!this.Cache.has(url)) {
-      this.Cache.set(url, new RealmProvider(url, urlLegacy))
+      this.Cache.set(url, new RealmProvider(url))
     }
     return this.Cache.get(url)!
   }
 
   static get() {
-    return this.from(this.Url, this.UrlLegacy)
+    return this.from(this.Url)
   }
 
-  async getHotScenes(isLegeacy = false): Promise<HotScene[]> {
+  async getHotScenes(): Promise<HotScene[]> {
     const controller = new AbortController()
 
     const timeoutId = setTimeout(() => controller.abort(), Time.Second * 10)
 
     try {
-      const response = await fetch(
-        `${isLegeacy ? this.urlLegacy : this.url}hot-scenes`,
-        {
-          signal: controller.signal as RequestInit["signal"],
-        }
-      )
+      const response = await fetch(`${this.url}hot-scenes`, {
+        signal: controller.signal as RequestInit["signal"],
+      })
       if (!response.ok) {
         throw new Error(`Failed to fetch hot scenes: ${response.statusText}`)
       }
@@ -62,38 +52,10 @@ export default class RealmProvider {
   }
 }
 
-export const processScene = (
-  scene: HotScene,
-  sceneMap: Map<string, HotScene>
-) => {
-  const key = `${scene.baseCoords[0]},${scene.baseCoords[1]}`
-  if (sceneMap.has(key)) {
-    // If scene exists, sum the users count
-    const existingScene = sceneMap.get(key)!
-    sceneMap.set(key, {
-      ...existingScene,
-      usersTotalCount: existingScene.usersTotalCount + scene.usersTotalCount,
-    })
-  } else {
-    sceneMap.set(key, scene)
-  }
-}
-
 export const fetchHotScenesAndUpdateCache = async () => {
   try {
-    const [hotScenesLegacy, hotScenes] = await Promise.all([
-      RealmProvider.get().getHotScenes(true),
-      RealmProvider.get().getHotScenes(),
-    ])
-
-    // Create a Map to store scenes by their baseCoords
-    const scenesMap = new Map<string, HotScene>()
-
-    // Process both arrays
-    hotScenesLegacy.forEach((scene) => processScene(scene, scenesMap))
-    hotScenes.forEach((scene) => processScene(scene, scenesMap))
-
-    memory = Array.from(scenesMap.values())
+    const response = await RealmProvider.get().getHotScenes()
+    memory = response
   } catch (error) {
     memory = DEFAULT_HOST_SCENE
   }
