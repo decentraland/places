40a1b424eeea920217d8a74186f1719419ea6994
Braian Mellor <braianj@gmail.com>
2025-01-09 13:53:29 -0300

    fix: show like/dislike when loading place page (#578)
    * fix: show like/dislike when loading place page

--- src/pages/place.tsx
+++ src/pages/place.tsx
@@ -52,7 +52,9 @@ export default function PlacePage() {
     [location.search]
   )
 
-  const [placeRetrived, placeRetrivedState] = usePlaceFromParams(params)
+  const [placeRetrived, placeRetrivedState] = usePlaceFromParams(params, [
+    account,
+  ])
 
   const placeMemo = useMemo(
     () => (!placeRetrived ? [[]] : [[placeRetrived]]),
