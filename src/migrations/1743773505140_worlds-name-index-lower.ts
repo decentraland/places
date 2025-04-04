ed796730191b4a3bcdbd42146ba5b8d98545ba52
Braian Mellor <braianj@gmail.com>
2025-04-04 12:11:51 -0300

    feat: pr related

--- src/migrations/1743773505140_worlds-name-index-lower.ts
+++ src/migrations/1743773505140_worlds-name-index-lower.ts
@@ -1,12 +1,17 @@
 import { MigrationBuilder } from "node-pg-migrate"
 
+import PlaceModel from "../entities/Place/model"
+
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.createIndex("places", "LOWER(world_name)", {
-    name: "places_world_name_lower_idx",
+  pgm.dropIndex(PlaceModel.tableName, ["world_name"])
+  pgm.createIndex(PlaceModel.tableName, ["world_name"], {
     where: "world is true",
   })
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropIndex("places", "places_world_name_lower_idx")
+  pgm.dropIndex(PlaceModel.tableName, ["world_name"])
+  pgm.createIndex(PlaceModel.tableName, ["world_name"], {
+    where: "world is true",
+  })
 }
