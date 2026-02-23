1e7d6b2000a516bfe8438fa6f6f91a0ba340638e
Andres Morelos <6563162+AndresMorelos@users.noreply.github.com>
2026-02-23 15:30:07 -0500

    fix: Update SDK filtering logic in PlaceModel to support wildcard matching

--- src/entities/Place/model.ts
+++ src/entities/Place/model.ts
@@ -134,7 +134,9 @@ export default class PlaceModel extends Model<PlaceAttributes> {
         )}
         ${conditional(
           !!options.sdk,
-          SQL` AND (${a}.sdk = ${options.sdk} OR ${a}.sdk IS NULL)`
+          SQL` AND (${a}.sdk = ${options.sdk} OR ${a}.sdk LIKE ${
+            options.sdk + ".%"
+          }${options.sdk === "6" ? SQL` OR ${a}.sdk IS NULL` : SQL``})`
         )}
         ${conditional(
           !!options.ids?.length,
